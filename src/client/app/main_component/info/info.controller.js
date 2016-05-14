214f90f9ab291d4e9dfc75219e98d573b34a0ab0
webdevmaurice <webdevmaurice@gmail.com>
2016-05-15 01:19:53 +0400

    login

--- src/client/app/main_component/info/info.controller.js
+++ src/client/app/main_component/info/info.controller.js
@@ -1,7 +1,9 @@
 (function(){
     angular.module('seasafe')
-        .controller('infoController', [function(){
+        .controller('infoController', ['$http',function($http){
             var gvm = this;
+            gvm.data = [];
+            gvm.noms = ['Pression atmosphérique',"Niveau d'oxygen",'Efficence Moteur','Niveau battery', 'RPM'];
             console.info('infoController');
             $('#yourdiv').liteAccordion();
             $(function() {
@@ -52,5 +54,26 @@
                 Page.init();
 
             });
+
+            gvm.fnAtmos = function() {
+                console.log('PRESSURE');
+                $http({
+                    method: 'GET',
+                    url: 'server/webservice/pressure.php'
+                }).then(function successCallback(response) {
+                    // this callback will be called asynchronously
+                    // when the response is available
+                    console.log(response);
+                    gvm.data = response.data;
+                }, function errorCallback(response) {
+                    // called asynchronously if an error occurs
+                    // or server returns response with an error status.
+                    console.log(response);
+                });
+            }
+
+            setInterval(function(){
+                gvm.fnAtmos();
+            }, 2000)
         }]);
 })();
