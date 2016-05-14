c452fa29ac2eef49a1f8046dcf659ff8fa23e95f
HashHDM <hashim.hdm@gmail.com>
2016-05-14 09:29:06 +0400

    Struc Hashim

--- src/client/lib/weblibs.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by webcup2016 on 09/05/2016.
- * weblibs 1.0.0
- */
-
-angular.module('weblibs',['ngAnimate','ngMaterial','ngMdIcons','materialCalendar','pascalprecht.translate','ngRoute','chart.js', 'angularTypewrite']);
