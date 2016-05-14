2a83a1537e628be9afdf0b3d33265610ceeea309
webdevmaurice <webdevmaurice@gmail.com>
2016-05-15 01:19:41 +0400

    login

---
+++ src/server/webservice/pressure.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: WebCup
+ * Date: 15/05/2016
+ * Time: 00:26
+ */
+$data = array(rand(0,50), rand(50,100), rand(0,50), rand(20,25), rand(100, 150));
+print json_encode($data);
